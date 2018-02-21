55292fd864079e0f7898c01015c348a89245530e
stella <stella@gmail.com>
2018-02-21 10:13:04 +0900

    conflict test in second field

--- TeamProject/src/kr/co/test/HelloWorld.java
+++ TeamProject/src/kr/co/test/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Git World!!!");
 		System.out.println("This content is second branch");
+		System.out.println("Why are you serious?");
 	}
 }
