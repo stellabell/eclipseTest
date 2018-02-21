c9bd6317145c01a68c062b13002721cf53522ffd
stella <stella@gmail.com>
2018-02-21 10:04:45 +0900

    second contents

--- TeamProject/src/kr/co/test/HelloWorld.java
+++ TeamProject/src/kr/co/test/HelloWorld.java
@@ -3,5 +3,6 @@ package kr.co.test;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Git World!!!");
+		System.out.println("This content is second branch");
 	}
 }
