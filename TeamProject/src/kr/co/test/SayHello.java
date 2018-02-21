33dff9fd532e153fc4f2c4f1e5f0555d19f190eb
stella <stella@gmail.com>
2018-02-21 11:04:43 +0900

    commit

--- TeamProject/src/kr/co/test/SayHello.java
+++ TeamProject/src/kr/co/test/SayHello.java
@@ -3,10 +3,7 @@ package kr.co.test;
 public class SayHello {
 	public static void main(String[] args) {
 		System.out.println("Test to save at remote storage");
-<<<<<<< HEAD
 		System.out.println("conflict testing");
-=======
 		System.out.println("Modify the contents");
->>>>>>> refs/remotes/origin/master
 	}
 }
