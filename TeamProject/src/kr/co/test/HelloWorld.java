fb29a189fe43b48efa9b112c805d937ca12774d9
stella <stella@gmail.com>
2018-02-21 10:11:06 +0900

    conflict test

--- TeamProject/src/kr/co/test/HelloWorld.java
+++ TeamProject/src/kr/co/test/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Git World!!!");
 		System.out.println("This content is second branch");
+		System.out.println("This is conflict test~");
 	}
 }
