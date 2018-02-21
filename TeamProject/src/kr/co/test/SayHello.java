6b717a91c302cad4e9002d887f8b5f37d792a01b
stella <stella@gmail.com>
2018-02-21 10:58:00 +0900

    retry coflict testing

--- TeamProject/src/kr/co/test/SayHello.java
+++ TeamProject/src/kr/co/test/SayHello.java
@@ -3,5 +3,6 @@ package kr.co.test;
 public class SayHello {
 	public static void main(String[] args) {
 		System.out.println("Test to save at remote storage");
+		System.out.println("conflict testing");
 	}
 }
